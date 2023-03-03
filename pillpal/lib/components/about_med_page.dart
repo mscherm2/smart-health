4531adfee69e67ea3fe949cdf0bbe099a9520fb6
Kailey Connors <kaileyconnors@Kaileys-MacBook-Air.local>
2023-03-03 17:04:05 -0500

    about page with listview entries

--- pillpal/lib/components/about_med_page.dart
+++ pillpal/lib/components/about_med_page.dart
@@ -1,24 +1,38 @@
 import 'package:flutter/material.dart';
 
 class AboutMedPage extends StatelessWidget {
+  // TODO: parse get data here
+  final List<String> entries = <String>['A', 'B', 'C'];
+  final List<int> colorCodes = <int>[600, 300, 100];
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const Text('About Med Page'),
-          const Text('first line'),
-          Row(
-            mainAxisSize: MainAxisSize.min,
-            children: const [
-              Text('first item'),
-              SizedBox(width: 10),
-              Text('second item'),
-            ],
+    return Column(
+      children: [
+        AppBar(
+          title: Text("About My Medications", style: TextStyle(color: Colors.white),),
+          centerTitle: true,
+          backgroundColor: Color(0xff020887),
+
+        ),
+        SizedBox(height: 20),
+        Text("Use this page to see your medication details or add a new one!"),
+        SizedBox(height: 20),
+        Expanded(
+          child: ListView.separated(
+            padding: const EdgeInsets.all(8),
+            itemCount: entries.length,
+            itemBuilder: (BuildContext context, int index) {
+              return Container(
+                height: 50,
+                color: Colors.lightGreen[colorCodes[index % 3]],
+                child: Center(child: Text('Entry ${entries[index]}')),
+              );
+            },
+            separatorBuilder: (BuildContext context, int index) => const Divider(),
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
