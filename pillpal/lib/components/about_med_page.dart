51ce542a96fb23785c1666dfd5a20fe08d831aae
Kailey Connors <kaileyconnors@Kaileys-MacBook-Air.local>
2023-03-07 15:12:44 -0500

    med list now pulling from parse

--- pillpal/lib/components/about_med_page.dart
+++ pillpal/lib/components/about_med_page.dart
@@ -1,38 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:parse_server_sdk_flutter/parse_server_sdk.dart';
+
+import '../services/about_med_service.dart';
 
 class AboutMedPage extends StatelessWidget {
   // TODO: parse get data here
   final List<String> entries = <String>['A', 'B', 'C'];
+
   final List<int> colorCodes = <int>[600, 300, 100];
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        AppBar(
-          title: Text("About My Medications", style: TextStyle(color: Colors.white),),
-          centerTitle: true,
-          backgroundColor: Color(0xff020887),
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.primaryContainer,
+      body: FutureBuilder(
+          future: Future.wait([getUser(), getMeds()]),
+          builder: (context, AsyncSnapshot<List<dynamic>> snapshot) {
+            if (snapshot.data?[0].username != null) {
+              return Column(
+                children: [
+                  AppBar(
+                    title: Text("About My Medications", style: TextStyle(color: Colors.white),),
+                    centerTitle: true,
+                    backgroundColor: Color(0xff020887),
+                  ),
+                  SizedBox(height: 20),
+                  Text("Hello, ${snapshot.data?[0].get('username')}! Use this page to see your medication details or add a new one!"),
+                  SizedBox(height: 20),
+                  Expanded(
+                    child: ListView.separated(
+                      padding: const EdgeInsets.all(8),
+                      itemCount: snapshot.data?[1].length,
+                      itemBuilder: (BuildContext context, int index) {
+                        return Container(
+                          height: 100,
+                          color: Colors.lightGreen[colorCodes[index % 3]],
+                          child: Center(child: Text('Name: ${snapshot.data?[1][index].get('Name')}\nDescription: ${snapshot.data?[1][index].get('Desc')}\nDays: ${snapshot.data?[1][index].get('Days')}\nTime: ${snapshot.data?[1][index].get('Time')}', textAlign: TextAlign.center,)),
+                        );
+                      },
+                      separatorBuilder: (BuildContext context, int index) => const Divider(),
+                    ),
+                  ),
+                ],
+              );
+            } else {
+              return Text("Hello");
+            }
+          }
 
         ),
-        SizedBox(height: 20),
-        Text("Use this page to see your medication details or add a new one!"),
-        SizedBox(height: 20),
-        Expanded(
-          child: ListView.separated(
-            padding: const EdgeInsets.all(8),
-            itemCount: entries.length,
-            itemBuilder: (BuildContext context, int index) {
-              return Container(
-                height: 50,
-                color: Colors.lightGreen[colorCodes[index % 3]],
-                child: Center(child: Text('Entry ${entries[index]}')),
-              );
-            },
-            separatorBuilder: (BuildContext context, int index) => const Divider(),
-          ),
-        ),
-      ],
     );
   }
 }
---
+++ pillpal/lib/services/about_med_service.dart
@@ -0,0 +1,29 @@
+import 'package:parse_server_sdk_flutter/parse_server_sdk.dart';
+
+Future<ParseUser?> getUser() async {
+  ParseUser? currentUser = await ParseUser.currentUser() as ParseUser?;
+  return currentUser;
+}
+
+Future<List<ParseObject>> getMeds() async {
+  final QueryBuilder<ParseObject> parseQuery = QueryBuilder<ParseObject>(ParseObject('Medication'));
+  // `whereContains` is a basic query method that checks if string field
+  // contains a specific substring
+  final ParseUser? currentUser = await getUser();
+
+  parseQuery.whereContains('user_id', currentUser?.get('objectId'));
+
+  // The query will resolve only after calling this method, retrieving
+  // an array of `ParseObjects`, if success
+  final ParseResponse apiResponse = await parseQuery.query();
+
+  List<ParseObject> results = <ParseObject>[];
+
+  if (apiResponse.success && apiResponse.results != null) {
+    results = apiResponse.results as List<ParseObject>;
+  }
+  else {
+    results = [];
+  }
+  return results;
+}
